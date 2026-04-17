d73ac7023930556cf5c1148fa9932ee50b8841f4
gabrielemarchesano <marchesano.gabriele@gmail.com>
2026-04-17 17:33:39 +0200

    milestone 2: real time validation of username, password and description

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-import { useState } from "react"
+import { useMemo, useState } from "react"
+
+const letters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()-_=+[]{}|;:'\\\",.<>?/`~";
 
 function App() {
 
@@ -8,6 +12,30 @@ function App() {
   const [ specialization, setSpecialization ] = useState("Frontend");
   const [ experience, setExperience ] = useState("2");
   const [ description, setDescription ] = useState("Ciao");
+
+  const isUsernameValid = useMemo(() => {
+    return [...username].every(char => letters.includes(char.toLowerCase()) ||  numbers.includes(char)) && username.length >= 6
+  }, [username])
+  console.log(isUsernameValid)
+
+  const isPassWordValid = useMemo(() => {
+    return (
+      [...password].some(char => letters.includes(char.toLowerCase())) && 
+      [...password].some(char => numbers.includes(char)) && 
+      [...password].some(char => symbols.includes(char)) &&
+      password.length >= 8
+    )
+  }, [password])
+  console.log("Password valida: ", isPassWordValid)
+
+  const isDescriptionValid = useMemo(() => {
+    return (
+      description.trim().length > 100 && 
+      description
+      .trim().length < 1000
+    )
+  }, [description])
+  console.log("Descrizione valida: ", isDescriptionValid, description.length)
 
   const handleSubmit = event => {
     event.preventDefault();
@@ -45,11 +73,17 @@ function App() {
         <label>
           <p>Username</p>
           <input type="text" onChange={event => setUsername(event.target.value)} value={username} placeholder="Username"/>
+          {
+            isUsernameValid ? (<p style={{color: "green"}}>Username valido!</p>) : (<p style={{color: "red"}}>Username deve avere caratteri alfanumerici</p>)
+          }
         </label>
         
         <label>
           <p>Password</p>
           <input type="password" onChange={event => setPassword(event.target.value)} value={password} placeholder="Password"/>
+          {
+          isPassWordValid ? (<p style={{ color: "green" }}>Password valida!</p>) : (<p style={{ color: "red" }}>La password deve contenere almeno una lettera, un numero e un simbolo</p>)
+          }
         </label>
         
         <label>
@@ -70,6 +104,9 @@ function App() {
         <label>
           <p>Breve descrizione</p>
           <textarea onChange={event => setDescription(event.target.value)} value={description} placeholder="Breve descrizione"/>
+          {
+            isDescriptionValid ? (<p style={{ color: "green" }}>{description.length}/1000</p>) : (<p style={{color: "red"}}>La descrizione deve contenere tra i 100 e i 1000 caratteri</p>)
+          }
         </label>
 
         <div>
